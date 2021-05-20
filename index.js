d64acd3033bf44a75c9a8f4bece3508a61bfc881
Stephanie Blom <stephblom@hotmail.com>
2021-05-20 08:38:03 -0600

    fix string error

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 module.exports = function contact(obj) {
     if (typeof obj !== "object") throw new TypeError("typeof contact is ", typeof obj);
-    return string.replace(/\s/g, "");
+    return obj.replace(/\s/g, "");
   };
